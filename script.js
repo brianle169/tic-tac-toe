780db9d63ccb8728f11c42742250cde80ed5c9cb
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-14 17:01:17 +0700

    Add basic GameBoard() variables and methods

---
+++ script.js
@@ -0,0 +1,19 @@
+// GameBoard factory-function, serve as a foundational container for other elements
+function GameBoard() {
+  const rows = 3;
+  const cols = 3;
+  const board = [];
+
+  for (let row = 0; row < rows; row++) {
+    board[row] = []; // Each row in the board is an array
+    for (let col = 0; col < cols; col++) {
+      board[row].push(Cell()); // Push a cell into each row.
+    }
+  }
+
+  const getBoard = () => board;
+}
+
+// Cell factory-function, serve as an object creator for game cells, where user will click
+// to play a move
+function Cell() {}
