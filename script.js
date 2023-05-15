58fbc4a99ed3f66444a680d3b22d5fadfa1ccacb
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-15 16:41:28 +0700

    Redefine initial components in a tic-tac-toe game

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-// GameBoard factory-function, serve as a foundational container for other elements
-function GameBoard() {
+// Player: each player is also an object
+const Player = () => {};
+
+// Cell: each Cell is an object, we will need multiple cells -> We apply FactoryFunction
+const Cell = () => {
+  const markedValue = "";
+  const getMarkedValue = () => markedValue;
+  return { getMarkedValue };
+};
+
+// GameBoard: the board display of the tic-tac-toe game -> We will only need ONE board, hence we use Module as an IIFE
+const Gameboard = (() => {
   const rows = 3;
   const cols = 3;
   const board = [];
 
+  // Build a board;
   for (let row = 0; row < rows; row++) {
-    board[row] = []; // Each row in the board is an array
+    // Each row is an array of cells
+    board[row] = [];
     for (let col = 0; col < cols; col++) {
-      board[row].push(Cell()); // Push a cell into each row.
+      // Append cells inside each row
+      board[row].push(Cell());
     }
   }
 
   const getBoard = () => board;
 
   return { getBoard };
-}
+})();
 
-// Cell factory-function, serve as an object creator for game cells, where user will click
-// to play a move
-function Cell() {
-  let move = "";
+// GameController: used to control game logic (winning condition, check invalid input, etc.) -> We will only need ONE of this,
+// hence the module pattern
+const GameController = (() => {})();
 
-  const playMove = (player) => {
-    move = player;
-  };
-
-  const getMove = () => move;
-
-  return { getMove, playMove };
-}
+// DisplayController: closely related to the web. This module will take care of DOM manipulation and interaction.
+const DisplayController = (() => {})();
