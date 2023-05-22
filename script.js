aa61e8f55f6d4d6585ab5ffdb5724ef18b0d39a1
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-22 16:04:58 +0700

    Implement winning condition by row. Add coordinate attribute to Cell factory function. Add winning information attributes to GameController module

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 // Cell Factory.
 // Cell(): Used to initiate Cell objects, where each player's mark will be displayed.
-const Cell = () => {
+const Cell = (rowIndex, colIndex) => {
+  const coordinate = {
+    r: rowIndex,
+    c: colIndex,
+  };
   let markedValue = "";
+  let markedPlayer = "";
   let isChecked = false; // the current status of cell
   const checkMark = (player) => {
-    markedValue = player;
+    markedPlayer = player.name;
+    markedValue = player.mark;
     isChecked = true;
   };
+  const getCoordinate = () => coordinate;
   const getMarkedValue = () => markedValue;
+  const getMarkedPlayer = () => markedPlayer;
   const getCheckStatus = () => isChecked;
-  return { getMarkedValue, checkMark, getCheckStatus };
+  return {
+    getMarkedValue,
+    checkMark,
+    getCheckStatus,
+    getCoordinate,
+    getMarkedPlayer,
+  };
 };
 
 // GameBoard Module.
@@ -25,7 +39,7 @@ const GameBoard = (() => {
     board[row] = [];
     for (let col = 0; col < cols; col++) {
       // Append cells inside each row
-      board[row].push(Cell());
+      board[row].push(Cell(row, col));
     }
   }
 
@@ -37,11 +51,22 @@ const GameBoard = (() => {
 
   const boardIsFilled = () => board.every(rowIsFilled);
 
-  const addPlayerMove = (row, col, mark) => {
-    board[row][col].checkMark(mark);
+  const rowIsIdentical = (row) => {
+    const result =
+      rowIsFilled(row) &&
+      row.every(
+        (element) => element.getMarkedValue() === row[0].getMarkedValue()
+      );
+    return result;
   };
 
-  return { getBoard, addPlayerMove, boardIsFilled };
+  const rowCheck = () => board.some(rowIsIdentical);
+
+  const addPlayerMove = (row, col, player) => {
+    board[row][col].checkMark(player);
+  };
+
+  return { getBoard, addPlayerMove, boardIsFilled, rowCheck };
 })();
 
 // GameController Module.
@@ -55,13 +80,16 @@ const GameController = (() => {
     name: "Life",
     mark: "☻",
   };
-
+  let gameEnd = false;
+  let winner = "";
   let currentPlayer = playerOne; // playerOne will always go first.
   let currentTurn = `${currentPlayer.name}'s turn to move!`;
   let resultMessage = `What an intense game!`;
   const getCurrentPlayer = () => currentPlayer;
   const getCurrentTurnMessage = () => currentTurn;
   const getResultMessage = () => resultMessage;
+  const getGameEndStatus = () => gameEnd;
+  const getWinner = () => winner;
   const switchTurn = () => {
     currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
     currentTurn = `${currentPlayer.name}'s turn to move!`;
@@ -72,26 +100,30 @@ const GameController = (() => {
     !GameBoard.getBoard()[row][col].getCheckStatus();
 
   // Check whether the game ended with a win.
-  const gameWon = () => false;
+  const gameWon = () => GameBoard.rowCheck();
 
   // TIE: when the game board is filled but no one wins.
   const gameTie = () => GameBoard.boardIsFilled() && !gameWon();
 
-  const playRound = (row, col, mark) => {
+  const playRound = (row, col) => {
     // If the move is valid (no overlap) then we add move to cell.
     if (validMove(row, col)) {
-      GameBoard.addPlayerMove(row, col, mark);
-    }
-    // Then check winning condition. This will allow the game to stop right after the last input
-    // If the game is not ended yet, we switch player's turn
-    if (!gameTie() && !gameWon()) {
+      GameBoard.addPlayerMove(row, col, currentPlayer);
+      if (gameTie()) {
+        resultMessage = `It's a TIE!`;
+        currentTurn = `Game ended!`;
+        gameEnd = true;
+        return;
+      }
+      if (gameWon()) {
+        resultMessage =
+          currentPlayer.name === "Death" ? `You're DEAD!` : "You SURVIVED!";
+        winner = currentPlayer.name;
+        currentTurn = "Game ended!";
+        gameEnd = true;
+        return;
+      }
       switchTurn();
-    }
-
-    // If it ended in a TIE. We re-assign the messages.
-    else if (gameTie()) {
-      resultMessage = `It's a TIE!`;
-      currentTurn = `Game ended!`;
     }
   };
 
@@ -99,6 +131,8 @@ const GameController = (() => {
     getCurrentPlayer,
     getCurrentTurnMessage,
     getResultMessage,
+    getGameEndStatus,
+    getWinner,
     switchTurn,
     playRound,
   };
@@ -111,13 +145,24 @@ const DisplayController = (() => {
   const messageSpan = document.querySelector(".message");
   const resultSpan = document.querySelector(".result-message");
 
+  const disableBoard = () => {
+    Array.from(boardDiv.childNodes).forEach((node) => {
+      node.setAttribute("disabled", "");
+      if (GameController.getWinner() === "Death") {
+        node.style.backgroundColor = "black";
+      }
+    });
+  };
+
   // clickHandler() callback: event handler for each cell click.
   const clickHandler = (event) => {
-    const thisCellRow = event.target.dataset.row;
-    const thisCellCol = event.target.dataset.column;
-    const thisCellMark = GameController.getCurrentPlayer().mark;
-    GameController.playRound(thisCellRow, thisCellCol, thisCellMark);
+    const cRow = event.target.dataset.row;
+    const cCol = event.target.dataset.column;
+    GameController.playRound(cRow, cCol);
     paintBoard();
+    if (GameController.getGameEndStatus()) {
+      disableBoard();
+    }
   };
 
   // paintBoard: render the initial game board, and add event handler to each cell.
